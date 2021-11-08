58f5c4fe9097e452239a1f40a33bc85eb2866889
Janis Kovalevskis <janis.kovalevskis@gmail.com>
2021-11-08 16:54:31 +0200

    wip

--- collatz/collatz.py
+++ collatz/collatz.py
@@ -2,23 +2,47 @@
 # by Janis Kovalevskis (janis.kovalevskis@gmail.com) (c) 2021
 # all respective rights apply, use at your own risk.
 
-testProgram = "101010101001010101010"
+testString = reversed("1000000000")
+
+testProgram = [c for c in testString]
+
+# f(p) : (p >> 1) if (p[0] == 0) else (p << 1 + p + 1)
 
 def collatz(program):
-	return program
+	try:
+
+		print(f"program: {program}")
+
+		if(program[0] == '0'):
+			return collatz(program[1:])
+		elif(program[0] == '1'):
+			#p1 = program.
+			if(len(program) > 1):
+				raise Exception(f"Unimplemented yet....")
+			return program
+			
+
+		raise Exception(f"program contains invalid symbol: {program[0]}")
+
+	except Exception as e:
+		raise e
 
 
 def solver(program):
-	totalIterations = 0
-	while(len(progam) > 1):
+	totalIterations = int(0)
+	while(len(program) > 1):
 		program = collatz(program)
-		print program
-		totalIterations++
+		print(program)
+		totalIterations = totalIterations + 1
+		if(totalIterations > 10000):
+			print(f"Too many iterations!!!")
+			break
 
 	print(f"Total iterations: {totalIterations}")
 	return True
 
 def main():
+	print(f"Test program: {testProgram}")
 	solver(testProgram)
 	return 0
 
