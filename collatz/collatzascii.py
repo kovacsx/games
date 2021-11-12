29ec21ceaf72d56e95efbb2d479f7951f96edf35
Janis Kovalevskis <janis.kovalevskis@gmail.com>
2021-11-12 19:21:51 +0200

    restructuring a bit, nicer UI

--- collatz/collatzascii.py
+++ collatz/collatzascii.py
@@ -2,21 +2,32 @@ from collatz import collatz
 
 drawInvestigator = 1
 
-# testProgram = "0" * 20 + "1" * 20 + "0" * 20 + "1" * 20
-# testProgram = "1" * 1000
-# testProgram = "1" * 100 + "110000111100111101001111110111100111110010000111100100000101010111011000011100100100100101010110010000110111100101101110111001001001110001101110010011101010001010010001100101011011001001001001111010111001010101000000100101011001001001110011000111011001110001100001001101111111100010001110001001100100100010101111010001010011011111101011010011011101001101011000100001101001100010011100110111101001111000010101000100101111011010000010010110001001100010110110001101111101011100101000011010010000101011001100110110000011100111100100"
-# testProgram = "1" * 20 + "010101010101" + "1" * 20
-testProgram = "1" + "1" * 100  + "1" * 40 + "1"
+investigatorChar = 'I'
+clueChar = '-'
+suspectChar = '+'
+mastermindChar = 'M'
+
+
+# default test program
+testDecks = ["1" + "1" * 50  + "1" * 50 + "1", \
+			"1" + "1" * 50 + "0"  + "1" * 50 + "1"]
+
+printHeuristics = False
+
 # testProgram = "".join(['0' if r % 2 == 0 else '1' for r in range(1,20)])
 # testProgram = testString[::-1] # reverse bit order
 
 
-def solver(program):
+def GameOfInvestigatorAndTheMastermind(program):
 	totalIterations = int(0)
 	while(len(program) > 1):
 		count1s = program.count('1')
 		count0s = program.count('0')
-		print(f"Program ({count0s:4} : {count1s:4} : {count1s - count0s:4} : {len(program):4})\t: {str('*' * totalIterations * drawInvestigator)} {program}")
+
+		if printHeuristics:
+			print(f"Program ({count0s:4} : {count1s:4} : {count1s - count0s:4} : {len(program):4})\t: {str(investigatorChar * totalIterations * drawInvestigator)} {program.replace('0', clueChar).replace('1', suspectChar) }{mastermindChar}")
+		else:
+			print(f"{str(investigatorChar * totalIterations * drawInvestigator)}{program.replace('0', clueChar).replace('1', suspectChar) }{mastermindChar}")
 
 		program = collatz(program)
 
@@ -31,8 +42,10 @@ def solver(program):
 	return True
 
 def main():
-	print(f"Test program: {testProgram}")
-	solver(testProgram)
+
+	for game in testDecks:
+		GameOfInvestigatorAndTheMastermind(game)
+
 	return 0
 
 
